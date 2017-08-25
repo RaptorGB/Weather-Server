3bcdfe813af1a48a660affc14157d351fd71a7a9
Reece <reecemillsom@gmail.com>
2017-08-25 20:00:51 +0100

    adding-routes initial config for karma done and working

--- karma.conf.js
+++ karma.conf.js
@@ -9,37 +9,30 @@ module.exports = function(config) {
 
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['mocha', 'requirejs', 'chai'],
+    frameworks: ['mocha', 'chai', 'sinon-chai', 'dirty-chai'],
 
     // list of files / patterns to load in the browser
     files: [
-      {pattern: 'spec/**/*.js', included: false},
+      {pattern: 'spec/**/*.js', included: true},
       {pattern: 'src/**/*.js', included: false},
     ],
 
     // list of files to exclude
     exclude: [
+      'node_modules'
     ],
 
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
-      'src/**/*.js': ['babel'],
+      'src/**/*.js': ['coverage'],
       'spec/**/*.js': ['babel']
-    },
-
-
-    babelPreprocessor: {
-      options: {
-        presets: ['es2015'],
-        sourceMap: 'inline'
-      }
     },
 
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
+    reporters: ['verbose', 'coverage'],
 
     // web server port
     port: 9876,
@@ -49,7 +42,7 @@ module.exports = function(config) {
 
     // level of logging
     // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-    logLevel: config.LOG_DEBUG,
+    logLevel: config.LOG_INFO,
 
     // enable / disable watching file and executing tests whenever any file changes
     autoWatch: true,
@@ -64,9 +57,10 @@ module.exports = function(config) {
       'karma-chai',
       'karma-sinon-chai',
       'karma-dirty-chai',
-      'karma-requirejs',
       'karma-babel-preprocessor',
-      'karma-chrome-launcher'
+      'karma-chrome-launcher',
+      'karma-coverage',
+      'karma-verbose-reporter'
     ],
 
     // Continuous Integration mode
